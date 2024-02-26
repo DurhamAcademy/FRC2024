70abe0eb35436fd27c9202abdadcbfc464f45f37
Joshua Yoon <dbstjrgus27@gmail.com>
2024-02-26 15:48:58 -0500

    against the wall command stuffs please, please check over the angles and commands

--- src/main/java/frc/robot/commands/ShooterCommands.java
+++ src/main/java/frc/robot/commands/ShooterCommands.java
@@ -24,19 +24,18 @@ public class ShooterCommands {
      * @param drive
      * @return (hopefully) the angle in which the shooter has to be at to make the shot to speaker
      */
-    private double doTrig(Drive drive){ //the parameter is the robot, idk how to declare it
+    private double doTrig(Drive drive){ //the parameter is the robot, idk how to declare it, also this returns the angle
         Pose3d drivePose = new Pose3d(drive.getPose().getX(), drive.getPose().getY(), offset, shooterAngle);
         double distance = Math.sqrt(Math.pow(drivePose.relativeTo(speakerPos).getX(), 2) + Math.pow(drivePose.relativeTo(speakerPos).getY(), 2));
         return Math.asin((2.045-offset)/distance);
     }
     private void populateITM() { //im making separate methods for this because I am not sure how much adjustments you would have to make
-        for(double i = 0.2; i< 2.0; i+=0.2){
-            distanceToAngle.put(i, Math.atan(2.045/i));
+        for(double i = 0.2; i< 2.0; i+=0.2){  //these initial inputs are optimal angles when assuming the robot and the speaker are perpendicularly aligned
+            distanceToAngle.put(i, Math.atan(2.045/i)); //2.045 metres is the basically the height of the speaker hole 
         }
-
-
     }
     public static Command autoaim(){
+
 
 
 
