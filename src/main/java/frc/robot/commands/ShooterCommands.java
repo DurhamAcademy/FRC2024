b36a2ddf17e0ebe35190ad09109591cbe8eff003
KP <86213869+Kanishk-Pandey@users.noreply.github.com>
2024-02-26 17:11:44 -0500

    temporary
    Signed-off-by: KP <86213869+Kanishk-Pandey@users.noreply.github.com>

---
+++ src/main/java/frc/robot/commands/ShooterCommands.java
@@ -0,0 +1,13 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.RunCommand;
+import frc.robot.subsystems.shooter.Shooter;
+
+public class ShooterCommands {
+    public static Command shooterCommand(Shooter shooter, HoodIO hood){
+        return new RunCommand(() ->{
+            shooter.
+        })
+    }
+}
