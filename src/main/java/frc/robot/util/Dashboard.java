69d4fc2a5e023e9bbbbb559dfbb134b105a6a421
KP <86213869+Kanishk-Pandey@users.noreply.github.com>
2024-03-12 10:13:06 -0400

    dashboard initial stuff
    Signed-off-by: KP <86213869+Kanishk-Pandey@users.noreply.github.com>

--- src/main/java/frc/robot/util/Dashboard.java
+++ src/main/java/frc/robot/util/Dashboard.java
@@ -1,21 +1,15 @@
 package frc.robot.util;
 
-import edu.wpi.first.util.sendable.Sendable;
-import edu.wpi.first.util.sendable.SendableBuilder;
 import edu.wpi.first.wpilibj.shuffleboard.BuiltInWidgets;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
-import edu.wpi.first.wpilibj.smartdashboard.Field2d;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.subsystems.drive.Drive;
 import frc.robot.subsystems.drive.VisionIOReal;
 import frc.robot.subsystems.feeder.Feeder;
 import frc.robot.subsystems.intake.Intake;
-import frc.robot.subsystems.shooter.HoodIO;
 import frc.robot.subsystems.shooter.Shooter;
 import org.littletonrobotics.junction.networktables.LoggedDashboardChooser;
-
-//import static com.sun.tools.classfile.AccessFlags.Kind.Field;
 
 public class Dashboard {
     public Dashboard(
@@ -24,7 +18,7 @@ public class Dashboard {
             Shooter shooter,
             Feeder feeder,
             Intake intake,
-            HoodIO.HoodIOInputs hood,
+//            HoodIO.HoodIOInputs hood,
             VisionIOReal vision
     ) {
         main.addDouble("Flywheel RPM", shooter::getShooterVelocityRPM)
@@ -36,23 +30,17 @@ public class Dashboard {
                 .withWidget(BuiltInWidgets.kComboBoxChooser)
                 .withSize(2, 1)
                 .withPosition(2, 0);
-//        main.addCamera("Camera feed", ) FIGURE OUT NAME AND STUFF
-        SmartDashboard.putData("Swerve Drive", new Sendable() {
-            @Override
-            public void initSendable(SendableBuilder builder) {
-                builder.setSmartDashboardType("SwerveDrive");
-                builder.addDoubleProperty("Front Left Angle", () -> drive.shuffleboardMethod()[0].getAngle().getRadians(), null);
-                builder.addDoubleProperty("Front Right Angle", () -> drive.shuffleboardMethod()[1].getAngle().getRadians(), null);
-                builder.addDoubleProperty("Back Left Angle", () -> drive.shuffleboardMethod()[2].getAngle().getRadians(), null);
-                builder.addDoubleProperty("Back Right Angle", () -> drive.shuffleboardMethod()[3].getAngle().getRadians(), null);
-                builder.addDoubleProperty("Robot Angle", () -> drive.getRotation().getRadians(), null);
-            }
+        main.addCamera("Camera feed", "ShootSideCamera");
+        SmartDashboard.putData("Swerve Drive", builder -> {
+            builder.setSmartDashboardType("SwerveDrive");
+            builder.addDoubleProperty("Front Left Angle", () -> drive.shuffleboardMethod()[0].getAngle().getRadians(), null);
+            builder.addDoubleProperty("Front Right Angle", () -> drive.shuffleboardMethod()[1].getAngle().getRadians(), null);
+            builder.addDoubleProperty("Back Left Angle", () -> drive.shuffleboardMethod()[2].getAngle().getRadians(), null);
+            builder.addDoubleProperty("Back Right Angle", () -> drive.shuffleboardMethod()[3].getAngle().getRadians(), null);
+            builder.addDoubleProperty("Robot Angle", () -> drive.getRotation().getRadians(), null);
         });
-
-        main.add("Hood Angle Manager", hood.hoodPositionRad)
-                .withWidget(BuiltInWidgets.kEncoder);
-
-        main.add("Field", new Field2d());
+//        main.add("Hood Angle Manager", hood.hoodPositionRad)
+//                .withWidget(BuiltInWidgets.kEncoder);
     }
     ShuffleboardTab main = Shuffleboard.getTab("Main");
 }
